749ac8fe40c4572e84693216cd079f94c28ade79
Andy5am <38666859+Andy5am@users.noreply.github.com>
2018-07-15 18:14:35 -0600

    setters

--- src/grados.java
+++ src/grados.java
@@ -1,4 +1,11 @@
 public class grados {
     private String nameGrado;
 
+    public String getNameGrado() {
+        return nameGrado;
+    }
+
+    public void setNameGrado(String nameGrado) {
+        this.nameGrado = nameGrado;
+    }
 }
--- src/niveles.java
+++ src/niveles.java
@@ -1,6 +1,20 @@
 public class niveles {
     private String namenivel;
-    private String grados;
+    private String namegrado;
 
+    public String getNamenivel() {
+        return namenivel;
+    }
 
+    public void setNamenivel(String namenivel) {
+        this.namenivel = namenivel;
+    }
+
+    public String getNamegrado() {
+        return namegrado;
+    }
+
+    public void setNamegrado(String namegrado) {
+        this.namegrado = namegrado;
+    }
 }
